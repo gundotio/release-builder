5e9580c17e9615045ab0f08194604d37a69544ea
Steve Lacey <steve@steve.ly>
2023-01-26 15:18:17 +0800

    Detect next semver level from pr title; closes #1

--- build-changelog/next_version.py
+++ build-changelog/next_version.py
@@ -1,7 +1,7 @@
 import semver
 import sys
 
-release = len(sys.argv) > 2 and sys.argv[2] or "patch"
-version = semver.VersionInfo.parse(sys.argv[1].lstrip("v"))
+current_version = semver.VersionInfo.parse(sys.argv[1].lstrip("v"))
+next_release = len(sys.argv) > 2 and sys.argv[2] or "patch"
 
-print(getattr(version, f"bump_{release}")())
+print(getattr(current_version, f"bump_{next_release}")())
