a5f113135123a7ee2791f4bc9769737437e3b66c
Steve Lacey <steve@steve.ly>
2023-01-26 16:19:33 +0800

    Support square brackets for semver levels

--- build-changelog/next_release.py
+++ build-changelog/next_release.py
@@ -3,11 +3,11 @@ import sys
 next_release = "patch"
 
 for line in sys.stdin.readlines():
-    if "(major)" in line.lower() or "#major" in line.lower():
+    if "[major]" in line.lower() or "(major)" in line.lower() or "#major" in line:
         next_release = "major"
         break
 
-    if "(minor)" in line.lower() or "#minor" in line.lower():
+    if "[minor]" in line.lower() or "(minor)" in line.lower() or "#minor" in line:
         next_release = "minor"
 
 print(next_release)
